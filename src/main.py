025566304317b55434f512199caf00af9008f524
Dmitry <dmitri199817@gmail.com>
2022-10-19 23:49:14 +0600

    add background

--- src/main.py
+++ src/main.py
@@ -1,25 +1,45 @@
-import sys, pygame
-pygame.init()
+from turtle import color
+import pygame, sys
+from pygame.locals import *
 
-size = width, height = 320, 240
+def handleEvents():
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT: 
+            sys.exit()
+
+        elif event.type == KEYUP:
+            if event.key == K_ESCAPE:
+                sys.exit()
+
+width = 1280
+height = 800
 speed = [2, 2]
 black = 0, 0, 0
 
-screen = pygame.display.set_mode(size)
+pygame.init()
+screen = pygame.display.set_mode(size=(width, height))
+pygame.display.set_caption("Physics Simulator")
+clock = pygame.time.Clock()
 
 ball = pygame.image.load("src\intro_ball.gif")
-ballrect = ball.get_rect()
+ballRect = ball.get_rect()
+
+ground = pygame.Surface(size=(width, height / 6))
+ground.fill((0, 200, 0))
+groundRect = ground.get_rect()
 
 while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT: sys.exit()
-
-    ballrect = ballrect.move(speed)
-    if ballrect.left < 0 or ballrect.right > width:
+    handleEvents()
+            
+    ballRect = ballRect.move(speed)
+    if ballRect.left < 0 or ballRect.right > width:
         speed[0] = -speed[0]
-    if ballrect.top < 0 or ballrect.bottom > height:
+    if ballRect.top < 0 or ballRect.bottom > height:
         speed[1] = -speed[1]
 
-    screen.fill(black)
-    screen.blit(ball, ballrect)
-    pygame.display.flip()
+    screen.fill(color=(0, 191, 235))
+    screen.blit(ball, ballRect)
+    screen.blit(ground, (0, (height - (height / 6))))
+
+    pygame.display.update()
+    clock.tick(60)
