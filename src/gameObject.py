3542ab6b7825c1fcb1c0ba01999b3e6d9b560ded
dmitri1998171 <dmitri199817@gmail.com>
2022-10-22 23:47:02 +0600

    add object selection feature

--- src/gameObject.py
+++ src/gameObject.py
@@ -18,7 +18,6 @@ class GameObject:
         self.color_g = 0
         self.color_b = 0
 
-
     def randomizeColor(self):
         self.color_r = random.randint(0, 256)
         self.color_g = random.randint(0, 256)
@@ -26,7 +25,6 @@ class GameObject:
 
     def draw(self, screen):
         pass
-
 
     def move(self, dx, dy): 
         pass
@@ -62,7 +60,6 @@ class Rectangle(GameObject):
     def __init__(self, screen, x, y):
         super().__init__(screen, x, y)
         super().randomizeColor()
-        # super().update()
 
         self.x = x
         self.y = y
@@ -70,17 +67,7 @@ class Rectangle(GameObject):
         self.h = 100
 
     def draw(self, screen):
-        # self.x = x
-        # self.y = y
         self.rectangle = pygame.draw.rect(screen, (self.color_r, self.color_g, self.color_b), (self.x, self.y, self.w, self.h))
-
-    # @property
-    # def get_draw(self):
-    #    return self.rectangle
-
-    # @property
-    def getObject(self):
-        return self.rectangle
 
     def isIntersect(self, event):
         if (self.rectangle.collidepoint(event.pos)):
@@ -89,6 +76,3 @@ class Rectangle(GameObject):
         else:
             print("debug: isIntersect False")
             return False      
-
-    # def move(self, event):
-        # pygame.Rect.move_ip(self.circle, event.rel, 0)
