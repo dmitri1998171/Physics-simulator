98a247bb40837e26edbaa70ef084e03ba212660a
eto-ban <alihan_tezekbaev@outlook.com>
2022-10-23 20:32:13 +0600

    PS-30 Создание подменю -#done

--- src/main.py
+++ src/main.py
@@ -37,14 +37,16 @@ class Game:
 
         # pygame_gui buttons
         self.propertiesWindowsCount = 0
+        self.isPropertiesClose = False
 
         self.manager = pygame_gui.UIManager((screen_rev.width, screen_rev.height), '../ext/theme.json')
         self.manager_properties = pygame_gui.UIManager((screen_rev.width, screen_rev.height), '../ext/theme_properties.json')
-        self.button_size_x_menu = screen_rev.width / 100 * 2.95
-        self.button_size_y_menu = screen_rev.height / 100 * 2.23
 
-        print(self.button_size_x_menu)
-        print(self.button_size_y_menu)
+        self.button_size_x_menu = 75
+        self.button_size_y_menu = 35
+
+        self.properties_size_x_button = 150
+        self.properties_size_y_button = 40
         
         self.button_size_x_tools = 80
         self.button_size_y_tools = 80
@@ -188,59 +190,62 @@ class Game:
                      sys.exit()
             
             if event.type == pygame.MOUSEBUTTONDOWN:
-                self.isPropertiesClose = True
                 for i in range(len(self.objects)):            
                     if self.objects[i].isIntersect(event):
                         if event.button == 1:
                             self.selectedObject = self.objects[i]
                             self.isDragging = True
-
                             if(self.selectedObject.canDragging and self.isDragging):
                                 self.mouse_x, self.mouse_y = event.pos
                                 self.cursorObjectDelta[0] = self.selectedObject.x - self.mouse_x
                                 self.cursorObjectDelta[1] = self.selectedObject.y - self.mouse_y   
                         # MENU RBM
                         if event.button == 3:
-                            self.isPropertiesClose = False
                             self.propertiesWindowsCount = self.propertiesWindowsCount + 1
                             if self.propertiesWindowsCount >= 2:
-                                self.properties = 0
-                                #self.properties.hide()
-                                print(f'self.propertiesWindowsCount > 2:{self.propertiesWindowsCount}')
+                                self.propertiesWindowsCount = self.propertiesWindowsCount - 1
+                                self.properties.kill()
+                                self.isPropertiesClose = False
+                                print(f'self.propertiesWindowsCount  :{self.propertiesWindowsCount}\n self.isPropertiesClose = False')
                             if self.isPropertiesClose == False:
+                                self.isPropertiesClose = True
                                 self.selectedObject = self.objects[i]
-                                self.properties = pygame_gui.elements.UIWindow(pygame.Rect(self.selectedObject.x,self.selectedObject.y ,200,200),
+                                self.properties = pygame_gui.elements.UIWindow(pygame.Rect(self.selectedObject.x,self.selectedObject.y ,self.properties_size_x_button ,self.properties_size_y_button * 3),
                                                         window_display_title = 'Properties',
                                                         visible=True,
                                                         object_id=f"#caop",
                                                         manager=self.manager_properties)
-                                self.properties_edit_button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((0,0), (200,50)),
+                                self.properties_edit_button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((0,0), (self.properties_size_x_button,self.properties_size_y_button)),
                                                     text='Delete',
                                                     container=self.properties,
                                                     tool_tip_text = 'Delete obj',
                                                     manager=self.manager_properties)
-                                self.properties_fix_object_button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((0,50), (200,50)),
+                                self.properties_fix_object_button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((0,self.properties_size_y_button * 1), (self.properties_size_x_button,self.properties_size_y_button)),
                                                     text='Fix object',
                                                     container=self.properties,
                                                     tool_tip_text = 'Fix object1',
                                                     manager=self.manager_properties)
-                                self.properties_edit_button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((0,100), (200,50)),
+                                self.properties_edit_button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((0,self.properties_size_y_button * 2), (self.properties_size_x_button,self.properties_size_y_button)),
                                                     text='Info see',
                                                     container=self.properties,
                                                     tool_tip_text = 'OBJ Info see',
                                                     manager=self.manager_properties)
-                                print('Debug: RMB click')        
+                                print('Debug: RMB menu is open')        
                         break
 
 
             if event.type == pygame_gui.UI_WINDOW_CLOSE:
                 if event.ui_element == self.properties:
                     self.propertiesWindowsCount = self.propertiesWindowsCount - 1
-                    print("Window closed")
+                    print("Manual Window closed")
             
             if event.type == pygame.MOUSEBUTTONUP:
                 if event.button == 1:            
                     self.isDragging = False
+                    if self.isPropertiesClose == True:
+                        self.properties.kill()
+                        print("Window closed by isPropertiesClose == True to False!")
+                        self.isPropertiesClose = False
 
             if event.type == pygame.MOUSEMOTION:
                 if(self.selectedObject.canDragging and self.isDragging):
